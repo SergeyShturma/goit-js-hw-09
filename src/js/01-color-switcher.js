259b5d465f89498b8a29b8b410489568eac35f79
SergeyShturma <sergeyshturma@gmail.com>
2022-12-21 14:14:42 +0200

    first

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,23 @@
+let timerId = null;
+const start = document.querySelector('button[data-start]');
+const stop = document.querySelector('button[data-stop]');
+stop.disabled = true;
+start.addEventListener('click', onStart);
+function onStart() {
+  timerId = setInterval(() => {
+    start.disabled = true;
+    stop.disabled = false;
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+}
+
+stop.addEventListener('click', onStop);
+function onStop() {
+  start.disabled = false;
+  stop.disabled = true;
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
