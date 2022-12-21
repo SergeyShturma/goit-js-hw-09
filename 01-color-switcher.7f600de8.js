e4b130b4ae928977776319e4f36c28923e590d97
SergeyShturma <109282979+SergeyShturma@users.noreply.github.com>
2022-12-21 12:15:26 +0000

    Deploying to gh-pages from @ SergeyShturma/goit-js-hw-09@259b5d465f89498b8a29b8b410489568eac35f79 🚀

---
+++ 01-color-switcher.7f600de8.js
@@ -0,0 +1,2 @@
+let t=null;const e=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");d.disabled=!0,e.addEventListener("click",(function(){t=setInterval((()=>{e.disabled=!0,d.disabled=!1,document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),d.addEventListener("click",(function(){e.disabled=!1,d.disabled=!0,clearInterval(t)}));
+//# sourceMappingURL=01-color-switcher.7f600de8.js.map
