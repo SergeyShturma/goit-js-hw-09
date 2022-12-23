f5422e0a7ea22f741c6f44205ab3c0664982a8d4
SergeyShturma <sergeyshturma@gmail.com>
2022-12-23 10:50:56 +0200

    third

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
+import Notiflix from 'notiflix';
+
+const delay = document.querySelector('input[name=delay]');
+const step = document.querySelector('input[name=step]');
+const amount = document.querySelector('input[name=amount]');
+const create = document.querySelector('button');
+
+create.addEventListener('click', onClick);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+  return promise;
+}
+
+function onClick(e) {
+  e.preventDefault();
+
+  for (let i = 0; i < amount.value; i += 1) {
+    createPromise(i + 1, delay.value + i * step.value)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
 }
